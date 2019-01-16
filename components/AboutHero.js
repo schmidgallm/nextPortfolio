f16f96b85d12757fb2d62cdfa00e34c8acce637d
schmidgallm <schmidgallm@outlook.com>
2019-01-15 21:48:55 -0700

    reconfig about me page layout

--- components/AboutHero.js
+++ components/AboutHero.js
@@ -1,35 +1,77 @@
 import Link from 'next/link';
 
 const AboutHero = () => (
-	<div className="about container">
-		<h2>hello there</h2>
-		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis placeat accusantium id neque repellendus sed
-			nobis voluptatem tempore saepe reprehenderit vitae voluptates repudiandae at quisquam molestiae impedit
-			temporibus dolorum nemo odit, deserunt dicta minus cumque! Esse nulla eveniet suscipit in!
-		</p>
-		<Link href="/about">
-			<a className="btn btn-primary">Contact Me</a>
-		</Link>
-
+	<div className="about container text-center">
+		<h2 className="my-4">About Me</h2>
+		<div className="row">
+			<div className="col-6 text-right">
+				<h4>ASU Grad</h4>
+			</div>
+			<div className="col-6 text-left">
+				<p>
+					Recieved Bachelor of Arts from ASU. Here I focused in historical reasearch and quickly found my love
+					for learning and discovering.
+				</p>
+			</div>
+		</div>
+		<div className="row">
+			<div className="col-6 text-right">
+				<p>
+					Developed custom SQL reporting scripts, MSAccess applications, and PowerBI dashboards. Here I also
+					became thrusted into a web developer role as well.
+				</p>
+			</div>
+			<div className="col-6 text-left">
+				<h4>Buisness Analystis</h4>
+			</div>
+		</div>
+		<div className="row">
+			<div className="col-6 text-right">
+				<h4>Front End Developer</h4>
+			</div>
+			<div className="col-6 text-left">
+				<p>
+					Once I transitioned out of buisness analystis and into a web developer position, I quickly found how
+					much similiar it was to historical research. Finding answers to questions, searching stack overflow,
+					etc.
+				</p>
+			</div>
+		</div>
+		<div className="row">
+			<div className="col-6 text-right">
+				<p>
+					I wanted to keep pushing my knowledge of web development and take a more classical learning approach
+					to it. I learned more how to be a developer in six months than my two years of on work experience.
+				</p>
+			</div>
+			<div className="col-6 text-left">
+				<h4>Full Stack Dev Bootcamp Graduation</h4>
+			</div>
+		</div>
+		<div className="row">
+			<div className="col-6 text-right">
+				<h4>Director of E-Commerce Development</h4>
+			</div>
+			<div className="col-6 text-left">
+				<p>
+					After about four years of develpoment I took the lead role of E-Commerce development. Lead
+					development of two Mobile Apps using React-Native and several Web Apps using Electron.
+				</p>
+			</div>
+		</div>
 		<style jsx>{`
 			.about {
-				text-align: center;
-				background-color: whitesmoke;
-				box-sizing: border-box;
-				padding: 20vh 0;
+				margin-top: 100px;
+				width: 50%;
 			}
-			h2 {
-				display: inline-block;
-				border-bottom: 1px solid #9b8650;
-				margin: 0;
+			.text-left {
+				border-left: 1px solid #9b8650;
 			}
-			p {
-				width: 75%;
-				margin: 30px auto;
+			.text-right {
+				border-right: 1px solid #9b8650;
 			}
-			a {
-				margin: 0;
+			h4 {
+				color: #9b8650;
 			}
 		`}</style>
 	</div>
--- pages/about.js
+++ pages/about.js
@@ -5,18 +5,8 @@ import AboutHero from '../components/AboutHero';
 export default () => (
 	<Layout>
 		<View>
-			<div className="about-container">
-				<AboutHero />
-			</div>
+			<AboutHero />
 		</View>
-		<style jsx>{`
-			.about-container {
-				position: absolute;
-				top: 50%;
-				left: 50%;
-				transform: translate(-50%, -50%);
-				width: 75%;
-			}
-		`}</style>
+		<style jsx>{``}</style>
 	</Layout>
 );
