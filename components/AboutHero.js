0f1d24f18c70f387f09c87a80efda8a29b6d5739
schmidgallm <schmidgallm@outlook.com>
2019-01-16 23:07:01 -0700

    i hate talking about myself. more about me component reconfiging

--- components/AboutHero.js
+++ components/AboutHero.js
@@ -1,45 +1,57 @@
 import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => {
+	NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+	NProgress.done();
+};
 
 const AboutHero = () => (
-	<div className="container text-center">
-		<div className="text-content">
-			<h2>About Me</h2>
-			<p>
-				Graduated from Arizona State University where I studied historical research. After graduation I started
-				as a business analysists developing custom SQL scripts along with MSAccess applications and Power BI
-				dashboards. Quite a jump from historical research but relativlily the same. Both involved quite a bit of
-				research before implementing anything.
-			</p>
-			<p>
-				Not long after that I was thrusted into web development creating little splash pages. It didnt take long
-				but I fell in love with programming afgter my first script. From there I took on to a full time Web
-				Developer
-			</p>
-			<p>
-				Soon enough I decided to get more of a classical education in it where I attended and graduated from
-				University of Arizona Coding Bootcamp. After graduation my skill set nearly trippled
-			</p>
-			<Link href="/about">
-				<a className="btn btn-primary mt-2">More about me</a>
-			</Link>
+	<div className="about-wrapper">
+		<div className="row">
+			<div className="col-lg-6 col-md-6 col-sm-12">
+				<div className="img-container">
+					<img className="img-responsive img-fluid" src="/static/thumb_portrait.png" />
+				</div>
+			</div>
+			<div className="col-lg-6 col-md-6 col-sm-12 mt-5">
+				<div className="about-content">
+					<h4>Developer. Mentor. Business Intellegint</h4>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, impedit sapiente ipsam culpa
+						totam repudiandae autem, corporis perspiciatis eaque doloribus veritatis commodi voluptas,
+						cumque reiciendis officia tenetur explicabo deserunt numquam nulla praesentium. Tenetur quos
+						corrupti numquam similique optio molestias nobis id praesentium quia veniam! Optio cum cumque
+						quidem dicta officiis.
+					</p>
+					<Link href="/contact">
+						<a className="btn btn-primary">Contact Me</a>
+					</Link>
+				</div>
+			</div>
 		</div>
-
 		<style jsx>{`
-			.text-content {
-				position: absolute;
-				top: 50%;
-				left: 50%;
-				transform: translate(-50%, -50%);
-				background-color: whitesmoke;
-				padding: 10vh 0;
+			.about-wrapper {
+				margin-top: 100px;
 			}
 			h2 {
 				display: inline-block;
 				border-bottom: 1px solid #9b8650;
 			}
-			p {
+			.img-container {
 				width: 75%;
-				margin: 30px auto;
+				margin: 0 auto;
+			}
+			img {
+				width: 100%;
+				margin: 0 auto;
+			}
+			.about-content {
+				width: 75%;
+				margin: 0 auto;
 			}
 		`}</style>
 	</div>
