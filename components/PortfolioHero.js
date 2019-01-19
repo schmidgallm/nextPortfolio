0823a7e658c9bd9cb3ee1b8c115237e78ae27ec2
schmidgallm <schmidgallm@outlook.com>
2019-01-18 23:55:41 -0700

    readdded state to portfoliohero component

--- components/PortfolioHero.js
+++ components/PortfolioHero.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
+import keys from '../config/keys';
+import axios from 'axios';
 
 class PortfolioHero extends Component {
+	state = {
+		repos: []
+	};
+
+	componentDidMount() {
+		// fetch all github repos and push them to state
+		axios({
+			method: 'GET',
+			datatType: 'json',
+			url: `https://api.github.com/users/schmidgallm/repos`,
+			data: {
+				client_id: keys.client_id,
+				client_secret: keys.client_secret
+			},
+			// in order to bring back topics we need to add below header (according to github api docs)
+			headers: {
+				Accept: 'application/vnd.github.mercy-preview+json'
+			}
+		}).then((response) => {
+			const repos = response.data;
+			console.log(repos);
+			this.setState({
+				repos
+			});
+		});
+	}
+
 	render() {
 		return (
 			<div className="portfolio container">
