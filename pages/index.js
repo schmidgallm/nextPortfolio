98350f6add740f7681a8e2b24ffc24b36bd1f981
schmidgallm <schmidgallm@outlook.com>
2019-01-22 21:18:00 -0700

    removed require from deleted component

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Hero from '../components/Hero';
 import TextCopy from '../components/TextCopy';
 import CardList from '../components/CardList';
 import Collaboration from '../components/Collaboration';
-import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 
 export default () => (
