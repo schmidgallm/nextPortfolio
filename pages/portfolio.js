051703e238e60b2446c4c63f5834b69b83301d09
schmidgallm <schmidgallm@outlook.com>
2019-01-23 23:43:12 -0700

    fixed bug for github api data

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -5,36 +5,11 @@ import fetch from 'isomorphic-fetch';
 import keys from '../config/keys';
 
 class portfolio extends React.Component {
-	static async getInitialProps() {
-		// const response = await fetch(`https://api.github.com/users/schmidgallm/repos`);
-		// const repos = await response.json();
-		// return { repos: repos };
-
-		// fetch all github repos and push them to state
-		const response = fetch('https://api.github.com/users/schmidgallm/repos', {
-			data: {
-				client_id: keys.client_id,
-				client_secret: keys.client_secret
-			},
-			headers: {
-				Accept: 'application/vnd.github.mercy-preview+json'
-			}
-		});
-		// const response = fetch({
-		// 	method: 'GET',
-		// 	datatType: 'json',
-		// 	url: `https://api.github.com/users/schmidgallm/repos`,
-		// 	data: {
-		// 		client_id: keys.client_id,
-		// 		client_secret: keys.client_secret
-		// 	},
-		// 	// in order to bring back topics we need to add below header (according to github api docs)
-		// 	headers: {
-		// 		Accept: 'application/vnd.github.mercy-preview+json'
-		// 	}
-		// });
-		const repos = await response;
-		return repos;
+	static async getInitialProps({ req }) {
+		const baseUrl = req ? `${req.protocol}://${req.get('Host')}` : '';
+		const response = await fetch(baseUrl + '/repos');
+		const repos = await response.json();
+		return { repos };
 	}
 
 	render() {
