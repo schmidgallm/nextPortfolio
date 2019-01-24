0386077c50fecee6047d8045d4533ee5243f4a5c
schmidgallm <schmidgallm@outlook.com>
2019-01-23 23:42:11 -0700

    added call to github api to designated route

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const router = express.Router();
 const Contacts = require('../models/Contacts');
+const axios = require('axios');
 
 router.post('/post', (req, res) => {
 	Contacts.create(req.body)
@@ -13,4 +16,22 @@ router.post('/post', (req, res) => {
 		});
 });
 
+router.get('/repos', (req, res) => {
+	axios({
+		method: 'GET',
+		datatType: 'json',
+		url: `https://api.github.com/users/${process.env.user_id}/repos`,
+		data: {
+			client_id: process.env.client_id,
+			client_secret: process.env.client_secret
+		},
+		// in order to bring back topics we need to add below header (according to github api docs)
+		headers: {
+			Accept: 'application/vnd.github.mercy-preview+json'
+		}
+	}).then((response) => {
+		const repos = response.data;
+		return res.json(repos);
+	});
+});
 module.exports = router;
