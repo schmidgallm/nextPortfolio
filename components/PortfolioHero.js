85aeed1307179f7d371d4493eff04cdeee3d7455
schmidgallm <schmidgallm@outlook.com>
2019-02-08 21:25:51 -0700

    deleted query wrapper and made stateless component

--- components/PortfolioHero.js
+++ components/PortfolioHero.js
@@ -1,104 +1,91 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
-import { gql } from 'react-apollo';
-
-const getRepos = gql`
-	query getRepos {
-		repos {
-			name
-			description
-			clone_url
-			stargazers_count
-			homepage
-			topics {
-				name
-			}
-		}
-	}
-`;
+import gql from 'graphql-tag';
+// import getRepos from '../queries/queries';
 
 class PortfolioHero extends Component {
+	getInitialProps() {
+		console.log(props);
+	}
 	render() {
 		return (
-			<Query query={getRepos}>
-				<div className="portfolio container">
-					<div className="row port-row">
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-react" />
-							<h4>React</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-react" />
-							<h4>GraphQL</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-node-js" />
-							<h4>Node</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-react" />
-							<h4>React-Native</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-aws" />
-							<h4>DevOps</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-react" />
-							<h4>Next</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fas fa-database" />
-							<h4>MySQL</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fas fa-database" />
-							<h4>MongoDB</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-node-js" />
-							<h4>Electron</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-js" />
-							<h4>Express</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-js" />
-							<h4>API Development</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-js" />
-							<h4>Jquery</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fas fa-fire" />
-							<h4>Firebase</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-js" />
-							<h4>ThreeJS</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-js" />
-							<h4>AJAX</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-js" />
-							<h4>HandleBars</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-wordpress" />
-							<h4>WordPress</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fas fa-mobile-alt" />
-							<h4>Responsive-Design</h4>
-						</div>
-						<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-							<i className="fab fa-css3-alt" />
-							<h4>CSS / Styled Components</h4>
-						</div>
+			<div className="portfolio container">
+				<div className="row port-row">
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-react" />
+						<h4>React</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-react" />
+						<h4>GraphQL</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-node-js" />
+						<h4>Node</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-react" />
+						<h4>React-Native</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-aws" />
+						<h4>DevOps</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-react" />
+						<h4>Next</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fas fa-database" />
+						<h4>MySQL</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fas fa-database" />
+						<h4>MongoDB</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-node-js" />
+						<h4>Electron</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-js" />
+						<h4>Express</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-js" />
+						<h4>API Development</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-js" />
+						<h4>Jquery</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fas fa-fire" />
+						<h4>Firebase</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-js" />
+						<h4>ThreeJS</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-js" />
+						<h4>AJAX</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-js" />
+						<h4>HandleBars</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-wordpress" />
+						<h4>WordPress</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fas fa-mobile-alt" />
+						<h4>Responsive-Design</h4>
+					</div>
+					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
+						<i className="fab fa-css3-alt" />
+						<h4>CSS / Styled Components</h4>
 					</div>
 				</div>
 				<style jsx>{`
@@ -124,7 +111,7 @@ class PortfolioHero extends Component {
 						color: black;
 					}
 				`}</style>
-			</Query>
+			</div>
 		);
 	}
 }
