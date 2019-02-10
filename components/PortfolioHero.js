878039ab09bf46596345a4416335adec0520a2f6
schmidgallm <schmidgallm@outlook.com>
2019-02-09 22:38:20 -0700

    turned to functinoal component

--- components/PortfolioHero.js
+++ components/PortfolioHero.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class PortfolioHero extends Component {
 	render() {
-		console.log(this.props);
 		return (
 			<div className="portfolio container">
 				<div className="row port-row">
