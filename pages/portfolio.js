78958992aa7a557d9f9cd93d5d24cf0c1b07ebfd
schmidgallm <schmidgallm@outlook.com>
2019-02-09 22:41:29 -0700

    add loadable suspense...server side render equivelant to react suspense

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -4,7 +4,9 @@ import PortfolioHero from '../components/PortfolioHero';
 import withData from '../lib/with-apollo-client';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import loadable from '@loadable/component';
 
+// graphql query
 const query = gql`
 	{
 		repos {
@@ -20,10 +22,15 @@ const query = gql`
 	}
 `;
 
+// loading component to load when ready
+// need to use loadable package not react suspense for server side rendering
+const LoadingComponent = loadable(() => import('../components/Loading'));
+
 const portfolio = ({ data }) => (
 	<Layout>
 		<View>
-			<PortfolioHero repos={data} />
+			{console.log(data)}
+			{data.loading ? <LoadingComponent /> : <PortfolioHero repos={data} />}
 		</View>
 	</Layout>
 );
