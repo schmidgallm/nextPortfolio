ef9d5835ac60100e2eb3d7cdef2a62ab76b29414
schmidgallm <schmidgallm@outlook.com>
2019-02-10 21:50:02 -0700

    redesing portfolio page for better ui

--- components/PortfolioHero.js
+++ components/PortfolioHero.js
@@ -7,111 +7,37 @@ class PortfolioHero extends Component {
 	}
 
 	render() {
-		const getTopic = (e) => {
-			console.log(e.target.id);
-		};
-
 		return (
-			<div className="portfolio container">
-				<div className="row port-row">
-					<div id="react" className="col-lg-3 col-md-4 col-sm-6 text-center port-item" onClick={getTopic}>
-						<i className="fab fa-react" />
-						<h4>React</h4>
+			<div className="portfolio container-fluid">
+				{console.log(this.props.repos)}
+				<div className="row">
+					<div className="col col-2 topic-nav">
+						<div className="logo">
+							<p>Portfolio</p>
+						</div>
 					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-react" />
-						<h4>GraphQL</h4>
+					<div className="col col-3">
+						<p>test</p>
 					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-node-js" />
-						<h4>Node</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-react" />
-						<h4>React-Native</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-aws" />
-						<h4>DevOps</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-react" />
-						<h4>Next</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fas fa-database" />
-						<h4>MySQL</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fas fa-database" />
-						<h4>MongoDB</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-node-js" />
-						<h4>Electron</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-js" />
-						<h4>Express</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-js" />
-						<h4>API Development</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-js" />
-						<h4>Jquery</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fas fa-fire" />
-						<h4>Firebase</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-js" />
-						<h4>ThreeJS</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-js" />
-						<h4>AJAX</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-js" />
-						<h4>HandleBars</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-wordpress" />
-						<h4>WordPress</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fas fa-mobile-alt" />
-						<h4>Responsive-Design</h4>
-					</div>
-					<div className="col-lg-3 col-md-4 col-sm-6 text-center port-item">
-						<i className="fab fa-css3-alt" />
-						<h4>CSS / Styled Components</h4>
+					<div className="col col-7">
+						<p>test</p>
 					</div>
 				</div>
 				<style jsx>{`
 					.portfolio {
-						margin: 100px auto;
+						margin-top: 100px;
+						height: calc(100vh - 100px);
 					}
-					.port-row {
-						display: flex;
-						justify-content: center;
+					.col {
+						margin: 0;
+						padding: 0;
+						height: calc(100vh - 100px);
 					}
-					.port-item {
+					.topic-nav {
 						background-color: whitesmoke;
-						padding: 30px 20px;
-						cursor: pointer;
-						margin: 10px;
-						border: 1px solid #9b8650;
 					}
-					.port-item:hover {
-						background-color: #9b8650;
-					}
-					i {
-						font-size: 25px;
-						color: black;
+					.logo {
+						border-bottom: 1px solid black;
 					}
 				`}</style>
 			</div>
