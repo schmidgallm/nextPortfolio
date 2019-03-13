32f31e967dd3e6472cfd94f76f2d62c062c87abe
schmidgallm <schmidgallm@outlook.com>
2019-03-12 20:47:40 -0700

    mapping through repo arrays to match clicked value

--- components/RepoItems.js
+++ components/RepoItems.js
@@ -13,21 +13,44 @@ class RepoItems extends Component {
       this.setState({ repoItem: e.target.value });
     };
 
-    const showRepos = () => {};
-
-    console.log(this.props);
+    console.log("working", this.props.repos.repos.repos);
+    const repos = this.props.repos.repos.repos;
+    const repoList = [];
+    repos.forEach(repo => {
+      repo.topics.forEach(topic => {
+        if (topic.name === this.state.repoItem) {
+          repoList.push(repo);
+        }
+      });
+    });
+    console.log(repoList);
     return (
       <div className="container-fluid my-5">
         <div className="row port-row">
           {this.props.topics.map(topic => {
             return (
-              <button key={topic} value={topic} className="btn btn-primary m-2">
+              <button
+                key={topic}
+                onClick={handleStateChange}
+                value={topic}
+                className="btn btn-primary m-2"
+              >
                 {topic}
               </button>
             );
           })}
         </div>
-
+        <div className="container my-5 repoItems">
+          <div className="row">
+            {repoList.map(repo => {
+              return (
+                <div className="col-lg-3 col-md-4 col-sm-6">
+                  <p>{repo.name}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
         <style jsx>{`
           .port-row {
             margin: 0 auto;
