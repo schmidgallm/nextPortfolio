5e08fd75e7222676b4a3c91c35975aa1006bcca7
schmidgallm <schmidgallm@outlook.com>
2019-03-12 21:44:21 -0700

    omg - route handling for comment post was commented out...now fixed. sorry for anyone who tried to message me

--- routes/routes.js
+++ routes/routes.js
@@ -1,73 +1,30 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-const db = require('../models/');
-const graphqlHTTP = require('express-graphql');
-const schema = require('../schema/schema');
+const db = require("../models/");
+const graphqlHTTP = require("express-graphql");
+const schema = require("../schema/schema");
 
 // GraphQL main endpoint
 router.use(
-	'/graphql',
-	graphqlHTTP({
-		schema,
-		graphiql: true
-	})
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
 );
-/*
-// post route for all contact form submits. posts to mlab in contact collection
-router.post('/post', (req, res) => {
-	Contacts.create(req.body)
-		.then((response) => {
-			console.log(response);
-			console.log('made it to router');
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-});
-*/
 
-/*
-// need to use axios and not isomporphic-fetch because we need to pass an object for github api to handle. fetch no likey objects except in callbacks.
-router.get('/repos', (req, res) => {
-	axios({
-		method: 'GET',
-		datatType: 'json',
-		url: `https://api.github.com/users/${process.env.user_id}/repos`,
-		data: {
-			client_id: process.env.client_id,
-			client_secret: process.env.client_secret
-		},
-		// github api needs below header in order to bring back repo topics
-		headers: {
-			Accept: 'application/vnd.github.mercy-preview+json'
-		}
-	}).then((response) => {
-		// return the data objet from the response and create each instance in db
-		const repos = response.data.filter((repo) => repo.topics.length != 0);
-		repos.forEach((repo) => {
-			db.Repos
-				.create({
-					_id: repo.id,
-					name: repo.name,
-					description: repo.description,
-					clone_url: repo.clone_url,
-					stargazers_count: repo.stargazers_count,
-					homepage: repo.homepage,
-					topics: repo.topics
-				})
-				.then((rsp) => {
-					console.log(rsp);
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		});
-		return res.json(repos);
-	});
+// post route for all contact form submits. posts to mlab in contact collection
+router.post("/post", (req, res) => {
+  db.Contacts.create(req.body)
+    .then(response => {
+      console.log(response);
+      console.log("made it to router");
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
-*/
 
 module.exports = router;
